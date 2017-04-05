ae736d64d6fa65330f13fc2a248d4ab1b00b9781
Kirill2017 <r470975@gmail.com>
2017-04-05 22:53:34 +0300

    Add files via upload

--- JavaApplication2.java
+++ JavaApplication2.java
@@ -18,26 +18,32 @@ public class JavaApplication2 {
             y = Integer.parseInt(args[1]);
         }
         
-        LocalDate firstDay = LocalDate.of(y, m, 1);
-        int day_of_week = firstDay.getDayOfWeek().getValue();
-        int LengthOfMonth = firstDay.lengthOfMonth();
-        int col = day_of_week - 1;
-        System.out.println("Mon   Tue   Wed   Thu   Fri   Sat   Sun");
-        for (int i = 0; i < col; i++)
-            System.out.print("      ");
-        for (int i = 1; i <= LengthOfMonth; i++)
-        {
-            if (i < 10)
-                System.out.print(" ");
-            System.out.print(" " + i + "   ");
-            col++;
-            if (col == 7)
-            {
-                col = 0;
-                System.out.println();
-            }
-        }
-        System.out.println();
-    }
+        Month month = new Month(y, m);
+        
+        System.out.println(month.print());
+//        LocalDate firstDay = LocalDate.of(y, m, 1);
+//        System.out.println(firstDay);
+//        System.out.println(firstDay.getDayOfWeek());
+//        int day_of_week = firstDay.getDayOfWeek().getValue();
+//        int LengthOfMonth = firstDay.lengthOfMonth();
+//        int col = day_of_week - 1;
+//        System.out.println("Mon   Tue   Wed   Thu   Fri   Sat   Sun");
+//        for (int i = 0; i < col; i++)
+//            System.out.print("      ");
+//        for (int i = 1; i <= LengthOfMonth; i++)
+//        {
+//            if (i < 10)
+//                System.out.print(" ");
+//            System.out.print(" " + i + "   ");
+//            col++;
+//            if (col == 7)
+//            {
+//                col = 0;
+//                System.out.println();
+//            }
+//        }
+//        System.out.println();
+            
+       }
     
 }
